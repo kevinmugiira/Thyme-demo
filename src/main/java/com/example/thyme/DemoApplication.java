2b44ba3cf4cdc27efa77f919746db3038a51af9c
Kevin Mugiira <kevinmugiira@gmail.com>
2022-06-27 08:39:30 +0300

    first commit

---
+++ src/main/java/com/example/thyme/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.thyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello Nyumbani!";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	
+
+}
